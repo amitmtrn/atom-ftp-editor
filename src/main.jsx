b5248503c5eccb20550e0137ed742970881f3714
amit <amit@mtrn.co.il>
2015-07-05 12:57:52 +0300

    add the download call and comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 var React = require("react");
 var nodePromises = require('node-promises');
+var Ftp = require('ftp');
 var fs = nodePromises('fs');
 
 module.exports = AtomReactStarter = {
   panelItem: {},
+  ftpClient: undefined,
   activate: function() {
     atom.commands.add('atom-workspace', {
       'ftp-editor:create': AtomReactStarter.createSettings
     });
-    // document.addEventListener("core:save", function(e) {
-    //   fs.existsPromise(atom.workspace.getActivePane().activeItem.buffer.file.path)
-    //     .then(function (exists) {
-    //       console.log(exists);s
-    //     });
-    // });
+    document.addEventListener("core:save", function(e) {
+      fs.existsPromise(atom.workspace.getActivePane().activeItem.buffer.file.path)
+        .then(function (exists) { // upload the new file to the ftp host
+          console.log(exists);
+        });
+    });
   },
 
   createSettings: function() {
@@ -25,13 +27,20 @@ module.exports = AtomReactStarter = {
       if(!args[0]) {
         return fs.writeFile(settingsPath, '{}');
       } else {
-        return ['err - file already exists'];
+        return ['file already exists'];
       }
     }).then(function(args) {
       return require(settingsPath);
     }).then(function(data) {
-      console.log(data);
+      AtomReactStarter.ftpClient = new Ftp(data);
+      // Going through each file and check the modified date.
+      // if the file is newer the the local it download it.
+      AtomReactStarter.ftpClient.on('ready', AtomReactStarter.getAllData);
     });
+  },
+
+  getAllData: function() {
+
   },
 
   deactivate: function() {
